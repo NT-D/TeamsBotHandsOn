66058f4cf4dc65e17cd1a3fb785a200c65dda0b6
Masayuki Ota <masota@microsoft.com>
2017-05-19 15:16:52 +0900

    Added menu and some dialogs

---
+++ TeamsBot/TeamsBot/Dialogs/LuisCallDialog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+
+
+namespace TeamsBot.Dialogs
+{
+    public class LuisCallDialog : IDialog<object>
+    {
+        public Task StartAsync(IDialogContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- TeamsBot/TeamsBot/Dialogs/RootDialog.cs
+++ TeamsBot/TeamsBot/Dialogs/RootDialog.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Connector;
@@ -8,24 +9,67 @@ namespace TeamsBot.Dialogs
     [Serializable]
     public class RootDialog : IDialog<object>
     {
-        public Task StartAsync(IDialogContext context)
-        {
-            context.Wait(MessageReceivedAsync);
+        private const string FlightMenu = "Book Flight";
+        private const string HotelMenu = "Book Hotel";
+        private List<string> mainMenuList = new List<string>() { FlightMenu, HotelMenu };
+        private string location;
 
-            return Task.CompletedTask;
+        public async Task StartAsync(IDialogContext context)
+        {
+            await context.PostAsync("Welcome to Root Dialog");
+            context.Wait(MessageReceiveAsync);
         }
 
-        private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
+        private async Task MessageReceiveAsync(IDialogContext context, IAwaitable<IMessageActivity> result)
         {
-            var activity = await result as Activity;
+            var reply = await result;
+            if (reply.Text.ToLower().Contains("help"))
+            {
+                await context.PostAsync("You can implement help menu here");
+            }
+            else
+            {
+                await ShowMainmenu(context);
+            }
+        }
 
-            // calculate something for us to return
-            int length = (activity.Text ?? string.Empty).Length;
+        private async Task ShowMainmenu(IDialogContext context)
+        {
+            //Show menues
+            PromptDialog.Choice(context, this.CallDialog, this.mainMenuList, "What do you want to do?");
+        }
 
-            // return our reply to the user
-            await context.PostAsync($"You sent {activity.Text} which was {length} characters");
+        private async Task CallDialog(IDialogContext context, IAwaitable<string> result)
+        {
+            //This method is resume after user choise menu
+            var selectedMenu = await result;
+            switch (selectedMenu)
+            {
+                case FlightMenu:
+                    //Call child dialog without data
+                    context.Call(new FlightDialog(), ResumeAfterDialog);
+                    break;
+                case HotelMenu:
+                    //Call child dialog with data
+                    context.Call(new HotelDialog(location), ResumeAfterDialog);
+                    break;
+            }
+        }
 
-            context.Wait(MessageReceivedAsync);
+        private async Task ResumeAfterDialog(IDialogContext context, IAwaitable<object> result)
+        {
+            //Resume this method after child Dialog is done.
+            var test = await result;
+            if (test != null)
+            {
+                location = test.ToString();
+            }
+            else
+            {
+                location = null;
+            }
+            await this.ShowMainmenu(context); // If you want to show main menu when the dialog is done, please comment out this line.
+            //context.Wait(MessageReceiveAsync);
         }
     }
 }
